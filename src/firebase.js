709d6a55a996b3bb7d050eff98add902ebb5693b
demersdesigns <paul@pdemersjr.com>
2017-12-07 17:53:38 -0500

    Adding Firebase skeleton

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC-O1NS5ZiPXXFns92BKkwbkcUqoMG0EHs",
+  authDomain: "new-tab-links.firebaseapp.com",
+  databaseURL: "https://new-tab-links.firebaseio.com",
+  projectId: "new-tab-links",
+  storageBucket: "new-tab-links.appspot.com",
+  messagingSenderId: "394726225043"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
